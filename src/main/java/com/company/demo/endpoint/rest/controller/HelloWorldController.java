9f3d5249ff822c5c03d6e4c51e2be4765363e121
RaJharit77 <hei.rajo.2@gmail.com>
2025-06-11 11:58:55 +0300

    feat: Add SendEmailRequested.java, SendEmailRequestedService.java and Update HelloWorldController.java

--- src/main/java/com/company/demo/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/company/demo/endpoint/rest/controller/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.company.demo.endpoint.rest.controller;
 
-import com.company.demo.service.HelloWorldService;
+import com.company.demo.mail.Email;
+import com.company.demo.mail.Mailer;
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-    private final HelloWorldService service;
+    private final Mailer mailer;
 
     @GetMapping("/hello")
-    public String helloWorld(@RequestParam String name) {
-        return service.uploadHelloWorldMessage(name);
+    @SneakyThrows
+    public String helloWorld(@RequestParam String to) {
+        var email =
+                new Email(new InternetAddress(to), List.of(), List.of(), "Hello world", "... world!", List.of());
+
+        mailer.accept(email);
+        return "... world!";
     }
 }
 
